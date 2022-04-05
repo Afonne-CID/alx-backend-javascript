712686e9a7d571681e34d22ad3d6d2dd06f8d688
Afonne-CID <delightinbusiness@gmail.com>
2022-04-05 07:33:45 +0000

    err fixed

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,8 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-
 export default function createReportObject(employeesList) {
-	return `allEmployees: { ${...eimployeesList} };
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
