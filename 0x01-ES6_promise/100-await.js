e8a375a1acf0f5d05cb182b8eb737d86d972fcda
Afonne-CID Paul Onyedikachi <75165644+Afonne-CID@users.noreply.github.com>
2022-04-06 19:33:47 +0100

    await in ES6

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils';
+
+
+export default async function asyncUploadUser() {
+  try {
+    const thePhoto = await uploadPhoto ();
+    const theUser = await createUser ();
+
+    return {
+      photo: thePhoto,
+      user: theUser,
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
