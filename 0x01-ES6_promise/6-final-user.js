2c4cc69bb0cc93681047ec3162692ce00f8d9c0c
Afonne-CID Paul Onyedikachi <75165644+Afonne-CID@users.noreply.github.com>
2022-04-06 17:43:34 +0100

    add `async` keyword

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
 	const res = [];
 	try {
 		const outcome = await signUpUser(firstName, lastName);
 		res.push({ status: 'fulfilled', value: outcome });
                 await uploadPhoto(fileName);
-	} catch(err) {
+	} catch (err) {
 		res.push( { 
 			status: 'rejected',
 			value: `Error: ${fileName} cannot be processed`,
