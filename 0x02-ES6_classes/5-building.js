647a61b21c951b32a0238976d255fc6d3eb7b661
Afonne-CID <delightinbusiness@gmail.com>
2022-04-07 16:34:17 +0000

    altered

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,8 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
