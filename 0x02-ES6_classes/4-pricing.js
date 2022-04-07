f81abc150d49154d3a1d3bdb40eb4fc60788f956
Afonne-CID <delightinbusiness@gmail.com>
2022-04-07 15:25:31 +0000

    static methods in ES6

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // `amount` Setter and Getter
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  // `currency` Setter and Getter
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static conversionRate(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
