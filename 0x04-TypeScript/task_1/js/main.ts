5778a9e7ccea3a6c078e25c044c67ff2894fe5f3
Afonne-CID <delightinbusiness@gmail.com>
2022-04-13 16:15:56 +0000

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,11 +1,21 @@
 interface Teacher {
   readonly firstName: string;
-  readonly lastName; string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
   [propName: string]: any;
 }
+
+// Creating a teacher and a director
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
 
 interface printTeacherFunction {
   firstName: string;
@@ -16,6 +26,15 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-function printTeacher(firstName, lastName): printTeacherFunction {
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
