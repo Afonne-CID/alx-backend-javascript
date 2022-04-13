40d861b7fdfb74eb86ee00057ea2a3d42b0853ef
Afonne-CID <delightinbusiness@gmail.com>
2022-04-13 16:35:23 +0000

    added class with methods

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -38,3 +38,42 @@ console.log(director1);
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
+
+console.log(printTeacher('Dora', 'Montero'));
+
+// Class interface
+interface studentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class constructor interface
+interface studentClassConstructor {
+  new (firstName: string, lastName: string): studentClassInterface;
+}
+
+// Create class and constructor via interface
+class StudentClass implements studentClassInterface{
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Class instance
+const student: StudentClass = new StudentClass('sofia', 'cheung');
+console.log(student.displayName());
+console.log(student.workOnHomework());
